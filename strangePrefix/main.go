a3df9d39a8af0a04aa8fcdf9073fc8f1ff453bfa
solipsis <david@keepkey.com>
2019-09-24 17:11:18 -0600

    strange numeric contstant prefix behavior

---
+++ strangePrefix/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := -+-5
+	b := -+-+5
+	c := -+-+-5
+	d := -+-+-+5
+	e := -+-+-+-5
+	f := -+-+-+-+5
+	g := -+-+-+-+-5
+
+	fmt.Println(a, b, c, d, e, f, g)
+
+}
